50e739bc5a4132b269789ff20630ae072c5ff3a9
aparkerson <112903911+aparkerson@users.noreply.github.com>
2024-12-09 18:49:18 -0800

    Update container.py

--- backend/apis/container.py
+++ backend/apis/container.py
@@ -1,1 +1,20 @@
-will have the ebpf here
+#!/bin/bash/python3
+
+#'container_type':'os_name','python version',['driver','set'],['special','ebpf','rules']
+container_types = {
+  'dev_nightly':('rhel-9.5','3.11',
+         [],['test']),
+  'prod':('rhel-9.4','3.10',
+         ['visual studio','gcc-devel'],['record history','monitor usage']),
+  'research':('rhel-9.4','3.8',
+         ['nvml','cuda-12.1'],['record history','monitor usage']),
+  'jupyter':('rhel-9.4','3.8',
+         ['jupyter'],['record history','monitor usage'])
+}
+
+ebpf_rules = {
+  'record history':'./ebpf',
+  'monitor usage':'',
+  'test':'./ebpf/test.py'
+}
+
